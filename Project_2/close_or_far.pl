48475eff51bcec485a5f52136d698bbe4317ba48
João Martins <up201707311@fe.up.pt>
2019-12-27 15:56:15 +0000

    finished board generation and solver

--- Project_2/close_or_far.pl
+++ Project_2/close_or_far.pl
@@ -2,7 +2,7 @@
 :- use_module(library(lists)).
 :- use_module(library(random)).
 % 0 - blank 1 - C (Close) 2 - F (Far)
-
+%%%%%%%%%%%%%%%%%%%%%%%% PUZZLE SOLVER %%%%%%%%%%%%%%%%%%%%%%%%%%%
 solver(Board,N,Opt) :-
   length(Board,N),
   NZeros is N-4,
@@ -26,11 +26,78 @@ constraintRows([R|Rs],N,NZeros) :-
   DF #= abs(I4 - I3),
   DC #< DF,
   constraintRows(Rs,N,NZeros).
+
+%%%%%%%%%%%%%%%%%%% BOARD GENERATION %%%%%%%%%%%%%%%%%%%%
+
+generate(N,NewB):-
+  solver(B,N,[variable(selRandom),value(selRandom)]),
+  createPuzzle(B,N,NewB),displayBoard(B).
+
+createPuzzle(B,N,NewB) :-
+  selectPieces(B,N,Pos,Types),
+  length(NewB,N),
+  buildBoard(NewB,N,Pos,Types).
+
+selectPieces(B,N,Pos,Types) :-
+  length(Pos,N),
+  length(Types,N),
+  domain(Pos,1,N),
+  domain(Types,1,2),
+  all_distinct(Pos),
+  constrainPiece(B,Pos,Types),
+  append(Pos,Types,Vars),
+  labeling([value(selRandom)],Vars).
+
+constrainPiece([],[],[]).
+constrainPiece([Row|Bs],[P|Ps],[1|Ts]) :-
+  element(P,Row,1),
+  constrainPiece(Bs,Ps,Ts).
+
+constrainPiece([Row|Bs],[P|Ps],[2|Ts]) :-
+  element(P,Row,2),
+  constrainPiece(Bs,Ps,Ts).  
+
+buildBoard([],_,[],[]).
+buildBoard([Row|Bs],N,[P|Ps],[T|Ts]) :-
+  length(Row,N),
+  nth1(P,Row,T),
+  buildBoard(Bs,N,Ps,Ts).
+
+%%%%%%%%%%%%%%%%%%%%%%%%% GENERATE AND SOLVE %%%%%%%%%%%%%
+
+close_or_far(N) :-
+  reset_timer,
+  generate(N,B),
+  print_time,
+  fd_statistics,
+  displayBoard(B),nl,nl,!,
+  reset_timer,
+  solver(B,N,[ffc]),
+  print_time,
+	fd_statistics,
+  displayBoard(B).
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%% STATISTICS %%%%%%%%%%%%%%%%%%%%%
+
+reset_timer :- statistics(walltime,_).	
+print_time :-
+	statistics(walltime,[_,T]),
+	TS is ((T//10)*10)/1000,
+	nl, write('Time: '), write(TS), write('s'), nl, nl.
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%% LABELING HEURISTICS %%%%%%%%%%%%%%%%%
   
-displayBoard([]) :- !,nl.
-displayBoard([R|Rs]) :-
-  write(R),nl,
-  displayBoard(Rs).
+selRandom(ListOfVars, Var, Rest) :-
+  random_select(Var, ListOfVars, Rest).
+
+selRandom(Var,_, BB0, BB1):- % selecionavalor de forma aleatória
+  fd_set(Var, Set), fdset_to_list(Set, List), random_member(Value,List), % da library(random)
+  (first_bound(BB0, BB1), Var#= Value;
+  later_bound(BB0, BB1), Var#\= Value).
+  
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TESTING %%%%%%%%%%%%%%%%%%%%%%%%%
 
 test :-
   Test = [
@@ -42,33 +109,28 @@ test :-
   solver(Test),
   displayBoard(Test).
 
-%%%%%%%%%%%%%%%%%%% BOARD GENERATION %%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% BOARD DISPLAY %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-generate(N,NewB):-
-  solver(B,N,[variable(selRandom),value(selRandom)]),
-  createPuzzle(B,NewB).
+displayBoard([]) :- !,nl.
+displayBoard([R|Rs]) :-
+  write('|'),
+  displayRow(R),
+  displayBoard(Rs).
 
-createPuzzle(B,NewB).% create
+displayRow([]) :- nl.
+displayRow([R|Rs]) :-
+  var(R),
+  write(' |'),
+  displayRow(Rs).
 
-selRandom(ListOfVars, Var, Rest) :-
-  random_select(Var, ListOfVars, Rest).
+displayRow([0|Rs]) :-
+  write(' |'),
+  displayRow(Rs).
 
-selRandom(Var, Rest, BB0, BB1):- % selecionavalor de forma aleatória
-  fd_set(Var, Set), fdset_to_list(Set, List), random_member(Value,List), % da library(random)
-  (first_bound(BB0, BB1), Var#= Value;
-  later_bound(BB0, BB1), Var#\= Value).
+displayRow([1|Rs]) :-
+  write('C|'),
+  displayRow(Rs).
 
-%%%%%%%%%%%%%%%%%%%%%%%%% GENERATE AND SOLVE %%%%%%%%%%%%%
-
-close_or_far(N) :-
-  generate(N,B),
-  displayBoard(B),nl,nl,!,
-  solver(B,N,[ffc]),
-  displayBoard(B).
-
-  
-  
-  
-  
-
-  
+displayRow([2|Rs]) :-
+  write('F|'),
+  displayRow(Rs).
